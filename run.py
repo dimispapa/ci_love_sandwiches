93d367089d0aa7574d6c82e91317f04d8cdce3df
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-06-18 10:11:32 +0000

    refactor: wrap the function calls in a main

--- run.py
+++ run.py
@@ -59,7 +59,14 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfuly.\n")
-    
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+print("Welcome to Love Sandwiches Data Automation")
+main()
